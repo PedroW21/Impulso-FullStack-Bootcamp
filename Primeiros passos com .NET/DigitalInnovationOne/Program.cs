9320cff0d1c297137d3f19069410ca711d2c0b8b
Pedro Verner <pedrow21br@gmail.com>
2022-04-14 08:15:43 -0300

    Escrevendo em .NET

--- Primeiros passos com .NET/DigitalInnovationOne/Program.cs
+++ Primeiros passos com .NET/DigitalInnovationOne/Program.cs
@@ -1,2 +1,7 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+
+int numeroVezes = 5;
+
+for(int i =0; i < numeroVezes; i++) {
+    Console.WriteLine("Bem Vindo ao curso de .NET!!"); // caso fosse so write ele não pularia linha automaticamente.
+}
