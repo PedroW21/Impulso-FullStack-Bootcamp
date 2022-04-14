8b0a6c6036bff88e8ad64ecd687167887fbaed5a
Pedro Verner <pedrow21br@gmail.com>
2022-04-14 08:16:15 -0300

    Fazendo interpolação com .net

--- Primeiros passos com .NET/DigitalInnovationOne/Program.cs
+++ Primeiros passos com .NET/DigitalInnovationOne/Program.cs
@@ -3,5 +3,5 @@
 int numeroVezes = 5;
 
 for(int i =0; i < numeroVezes; i++) {
-    Console.WriteLine("Bem Vindo ao curso de .NET!!"); // caso fosse so write ele não pularia linha automaticamente.
+    Console.WriteLine($"Bem Vindo ao curso de .NET!! {i}"); // caso fosse so write ele não pularia linha automaticamente.
 }
