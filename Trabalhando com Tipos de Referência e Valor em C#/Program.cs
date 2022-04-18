87be38ae87693a54ed2b4a29e8a66a96e4d649b6
Pedro Verner <pedrow21br@gmail.com>
2022-04-18 18:02:42 -0300

    Continuando

--- Trabalhando com Tipos de Referência e Valor em C#/Program.cs
+++ Trabalhando com Tipos de Referência e Valor em C#/Program.cs
@@ -1,2 +1,33 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using static System.Console;
+
+public class Program
+{
+
+    static void demon1()
+    {
+        int a = 2;
+
+        a = Adicionar20(a);
+
+        WriteLine($"O valor atual da variável é: {a}");
+    }
+    static int Adicionar20(int x)
+    {
+        return x + 20;
+    }
+
+    static void TrocarNome(Pessoa p1, string nomeNovo) {
+        p1.Nome = nomeNovo;
+    }
+    public static void Main()
+    {
+        Pessoa p1 = new Pessoa();
+        p1.Nome = "Pedro";
+        p1.Idade = 19;
+        p1.Documento = "1234";
+
+        TrocarNome(p1, "Pierre");
+
+        WriteLine($"O Novo nome é: {p1.Nome}");
+    }
+}
