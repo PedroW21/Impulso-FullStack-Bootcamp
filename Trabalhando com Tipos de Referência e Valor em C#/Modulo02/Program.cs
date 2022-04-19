ec4e43648132a6e3f45aab6933f298599a857326
Pedro Verner <pedrow21br@gmail.com>
2022-04-19 10:52:20 -0300

    Localizando e alterando o nome num array

--- Trabalhando com Tipos de Referência e Valor em C#/Modulo02/Program.cs
+++ Trabalhando com Tipos de Referência e Valor em C#/Modulo02/Program.cs
@@ -13,36 +13,55 @@ class TrabalhandoComRef
     {
         a += 20;
     }
-    
+
     static void AlterarNome(string[] nomes, string nome, string novoNome)
     {
-        for(int i = 0; i < nomes.Length; i++)
+        for (int i = 0; i < nomes.Length; i++)
         {
-            if(nomes[i] == nome) 
+            if (nomes[i] == nome)
             {
                 nomes[i] = novoNome;
             }
         }
     }
-    
+
+    static int LocalizarNome(string[] nomes, string nome)
+    {
+        for (int i = 0; i < nomes.Length; i++)
+        {
+            if (nomes[i] == nome) return i;
+        }
+
+        return -1;
+    }
     static void Main()
     {
-        var nomes = new string[]{"Pedro","Victor","Ana","Anny","Jao","Samuel"};
+        var nomes = new string[] { "Pedro", "Victor", "Ana", "Anny", "Jao", "Samuel" };
         WriteLine($@"A lista de nomes atuais é:
         {string.Join(", \n", nomes)}
         ");
 
 
         WriteLine("Digite o nome a ser substituido: ");
-        var nome =  ReadLine();
+        var nome = ReadLine();
         WriteLine("Digite o nome novo: ");
         var nomeNovo = ReadLine();
 
-        AlterarNome(nomes, nome, nomeNovo);
+        var indice = LocalizarNome(nomes, nome);
 
-        WriteLine($@"A lista de nomes alterada é:
-        {string.Join(", \n", nomes)}
+        if (indice >= 0)
+        {
+            nomes[indice] = nomeNovo;
+            WriteLine($@"A lista de nomes alterada é:
+            {string.Join(", \n", nomes)}
         ");
+        }
+        else
+        {
+            WriteLine("Nome não encontrado :/");
+        }
+
+
 
     }
 }
