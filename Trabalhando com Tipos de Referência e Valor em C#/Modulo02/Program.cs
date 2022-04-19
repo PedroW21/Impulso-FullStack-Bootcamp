47b57f4c5e2c25631a5851c2c6ee8ac5efa0921c
Pedro Verner <pedrow21br@gmail.com>
2022-04-19 10:38:21 -0300

    Criado e alterado um array de strings

--- Trabalhando com Tipos de Referência e Valor em C#/Modulo02/Program.cs
+++ Trabalhando com Tipos de Referência e Valor em C#/Modulo02/Program.cs
@@ -2,16 +2,47 @@
 
 class TrabalhandoComRef
 {
-    static void Adicionar20(ref int a)
-    {
-        a += 20;
-    }
-    static void Main()
+    static void Demo01()
     {
         int a = 5;
         Adicionar20(ref a);
 
         WriteLine($"O valor de a é: {a}!");
+    }
+    static void Adicionar20(ref int a)
+    {
+        a += 20;
+    }
+    
+    static void AlterarNome(string[] nomes, string nome, string novoNome)
+    {
+        for(int i = 0; i < nomes.Length; i++)
+        {
+            if(nomes[i] == nome) 
+            {
+                nomes[i] = novoNome;
+            }
+        }
+    }
+    
+    static void Main()
+    {
+        var nomes = new string[]{"Pedro","Victor","Ana","Anny","Jao","Samuel"};
+        WriteLine($@"A lista de nomes atuais é:
+        {string.Join(", \n", nomes)}
+        ");
+
+
+        WriteLine("Digite o nome a ser substituido: ");
+        var nome =  ReadLine();
+        WriteLine("Digite o nome novo: ");
+        var nomeNovo = ReadLine();
+
+        AlterarNome(nomes, nome, nomeNovo);
+
+        WriteLine($@"A lista de nomes alterada é:
+        {string.Join(", \n", nomes)}
+        ");
 
     }
 }
