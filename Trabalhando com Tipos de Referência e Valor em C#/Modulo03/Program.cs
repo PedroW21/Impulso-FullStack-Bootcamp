774215540cf813e3811b0044895823f462411ab2
Pedro Verner <pedrow21br@gmail.com>
2022-04-19 14:03:48 -0300

    Testando struct com ref para ele nunca ir para heap

---
+++ Trabalhando com Tipos de Referência e Valor em C#/Modulo03/Program.cs
@@ -0,0 +1,17 @@
+﻿using static System.Console;
+
+
+Pessoa p1 = new Pessoa();
+
+p1.Nome = "Pierre";
+p1.Idade = 19;
+p1.EnderecoPessoa = new Endereco() {
+    Logradouro = "Rua Iriney",
+    Cidade = "Irinestro",
+    CEP = "77777-000",
+    Numero = 1299
+};
+
+WriteLine("Fim XD");
+
+// Tiramos a necessidade do garbage colector, tornando a aplicação mais rápida (pois garantimos que ela não ira para HEAP);
