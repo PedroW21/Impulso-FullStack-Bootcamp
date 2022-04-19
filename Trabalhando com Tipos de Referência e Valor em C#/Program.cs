c34df6e4c0159a4c8116132ca70b5419ab6a200f
Pedro Verner <pedrow21br@gmail.com>
2022-04-19 08:41:50 -0300

    Entendendo Struct

--- Trabalhando com Tipos de Referência e Valor em C#/Program.cs
+++ Trabalhando com Tipos de Referência e Valor em C#/Program.cs
@@ -2,7 +2,23 @@
 
 public class Program
 {
+    static void demon2()
+    {
+        Pessoa p1 = new Pessoa();
+        p1.Nome = "Pedro";
+        p1.Idade = 19;
+        p1.Documento = "1234";
 
+        Pessoa p2 = p1.Clone();
+
+        TrocarNome(p1, "Pierre");
+
+        // o arroba aqui faz com que apareca essa formatação no console
+        WriteLine($@"
+        O nome de p1 é: {p1.Nome}
+        O nome de p2 é: {p2.Nome}
+        ");
+    }
     static void demon1()
     {
         int a = 2;
@@ -16,24 +32,31 @@ public class Program
         return x + 20;
     }
 
-    static void TrocarNome(Pessoa p1, string nomeNovo) {
+    static void TrocarNome(Pessoa p1, string nomeNovo)
+    {
         p1.Nome = nomeNovo;
     }
+    
+    static void TrocarNome(StructPessoa p1, string nomeNovo) {
+        p1.Nome = nomeNovo;
+    }
+    
     public static void Main()
     {
-        Pessoa p1 = new Pessoa();
-        p1.Nome = "Pedro";
-        p1.Idade = 19;
-        p1.Documento = "1234";
+        StructPessoa p1 = new StructPessoa() {
+            Documento = "1234",
+            Idade = 30,
+            Nome = "Pedro"
+        };
 
-        Pessoa p2 = p1.Clone();
+        var p2 = p1;
 
         TrocarNome(p1, "Pierre");
-       
-        // o arroba aqui faz com que apareca essa formatação no console
+
         WriteLine($@"
-        O nome de p1 é: {p1.Nome}
-        O nome de p2 é: {p2.Nome}
+        O nome de p1 é {p1.Nome}
+        O nome de p2 é {p2.Nome}
         ");
+
     }
 }
