feeb352b65034f46b321ad7a03a97f866e27cbb2
Pedro Verner <pedrow21br@gmail.com>
2022-05-12 18:37:59 -0300

    Capturando informações de um diretorio e mostrando, e tratando um erro simples caso tenha

--- Trabalhando com Arquivos e Streams em C#/ReadDir/Program.cs
+++ Trabalhando com Arquivos e Streams em C#/ReadDir/Program.cs
@@ -1,1 +1,39 @@
-﻿
+﻿using static System.Console;
+
+var path = @"~\Área de Trabalho\DIO\Impulso-FullStack-Bootcamp\Trabalhando c
+om Arquivos e Streams em C#\Directory_And_DirectoryInfo\Terra";
+
+LerDir(path);
+
+WriteLine("Pressione ENTER para finalizar...");
+
+ReadLine();
+
+static void LerDir(string path)
+{
+    if (Directory.Exists(path))
+    {
+        var diretorios = Directory.GetDirectories(path, "*", SearchOption.AllDirectories); // todos os diretorios, posso selecionar so os que começam com p assim: p*
+        foreach (var dir in diretorios)
+        {
+            var dirInfo = new DirectoryInfo(dir);
+
+            WriteLine($"[Nome]: {dirInfo.Name}");
+            WriteLine($"[Raiz]: {dirInfo.Root}");
+
+            if (dirInfo.Parent != null)
+                WriteLine($"[Pai]: {dirInfo.Parent.Name}");
+
+            WriteLine("--------------------&--------------------");
+        }
+    }
+    
+    else
+    
+    {
+        WriteLine($"{path} não existe!!");
+    }
+
+
+}
+
