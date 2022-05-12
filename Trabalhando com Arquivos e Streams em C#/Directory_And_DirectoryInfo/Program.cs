277b748220a0fe3704127cf19d4ce5cdf8aafd53
Pedro Verner <pedrow21br@gmail.com>
2022-05-12 17:53:48 -0300

    Criando uma estrutura de diretórios e subdir em um programa

--- Trabalhando com Arquivos e Streams em C#/Directory_And_DirectoryInfo/Program.cs
+++ Trabalhando com Arquivos e Streams em C#/Directory_And_DirectoryInfo/Program.cs
@@ -1,1 +1,32 @@
-﻿
+﻿using static System.Console;
+
+var path = Path.Combine(Environment.CurrentDirectory, "Terra");
+
+var dirGlobo = Directory.CreateDirectory(path);
+
+var dirAmNorte = dirGlobo.CreateSubdirectory("América do Norte");
+var dirAmCentral = dirGlobo.CreateSubdirectory("América Central");
+var dirAmSul = dirGlobo.CreateSubdirectory("América do Sul");
+
+dirAmNorte.CreateSubdirectory("Canada");
+dirAmNorte.CreateSubdirectory("USA");
+dirAmNorte.CreateSubdirectory("México");
+
+dirAmCentral.CreateSubdirectory("Costa Rica");
+dirAmCentral.CreateSubdirectory("Panama");
+dirAmCentral.CreateSubdirectory("El Salvador");
+dirAmCentral.CreateSubdirectory("Honduras");
+
+dirAmSul.CreateSubdirectory("Brasil");
+dirAmSul.CreateSubdirectory("Chile");
+dirAmSul.CreateSubdirectory("Colombia");
+dirAmSul.CreateSubdirectory("Argentina");
+dirAmSul.CreateSubdirectory("Uruguai");
+
+
+
+
+
+
+
+
