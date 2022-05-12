defa5a1504c6ccae7227e01dda39f944c8717c5e
Pedro Verner <pedrow21br@gmail.com>
2022-05-12 16:32:26 -0300

    Criando um  arquivo txt a partir de um programa

---
+++ Trabalhando com Arquivos e Streams em C#/SystemIO/Program.cs
@@ -0,0 +1,13 @@
+﻿var path = Path.Combine(Environment.CurrentDirectory, "text.txt");
+
+
+// caso eu colocasse o using antes do "var streamWriter...", o metodo contem uma interface que força a descarga da memoria, não necessitando do flush no codigo
+var streamWriter = File.CreateText(path);
+
+streamWriter.WriteLine("Oi! Fui escrito por um programa :D! Linha 1");
+streamWriter.WriteLine("Oi! Fui escrito por um programa :X! Linha 2");
+streamWriter.WriteLine("Oi! Fui escrito por um programa :P! Linha 3");
+
+// O metodo não foi encerrado de maneira correta, precisando do metodo abaixo para que de fato escreva no .txt
+
+streamWriter.Flush(); // Realiza a descarga da memoria no arquivo (podendo ser por linha, por bloco de texto ou so no final da escrita do arquivo)
