8f18dbddec2e16cd78eb1e7718972ff334446e43
Pedro Verner <pedrow21br@gmail.com>
2022-05-13 14:59:01 -0300

    Modelo onde o separador é diferente de virgula

---
+++ Trabalhando com Arquivos e Streams em C#/CsvHelperDio/Model/Livro.cs
@@ -0,0 +1,10 @@
+public class Livro
+{
+    public string Titulo { get; set; }
+    public string ISBN { get; set; }
+    public string Autor { get; set; }
+    public string Assunto { get; set; }
+    public string Valor { get; set; }
+
+
+}
--- Trabalhando com Arquivos e Streams em C#/CsvHelperDio/Program.cs
+++ Trabalhando com Arquivos e Streams em C#/CsvHelperDio/Program.cs
@@ -5,10 +5,42 @@ using static System.Console;
 
 // LerCsvDinamyc();
 
-LerCsvComClasse();
+//LerCsvComClasse();
+
+LerCsvComOutroDelimitador();
 
 WriteLine("Pressione ENTER para sair...");
 ReadLine();
+
+static void LerCsvComOutroDelimitador() {
+
+    var path = Path.Combine(Environment.CurrentDirectory, "Entrada", "atelie-catalogo-produtos.csv");
+
+    var fi = new FileInfo(path);
+
+    if (!fi.Exists) throw new FileNotFoundException($"O arquivo {path} não existe!!!"); 
+
+    using var sr = new StreamReader(fi.FullName);
+
+    var csvConfig = new CsvConfiguration(CultureInfo.InvariantCulture) 
+    {
+        Delimiter = ";"
+    };
+
+    using var csvReader = new CsvReader(sr, csvConfig);
+
+    var registros = csvReader.GetRecords<Livro>();
+
+    foreach (var registro in registros)
+    {
+        WriteLine($"titulo: {registro.Titulo}");
+        WriteLine($"isbn: {registro.ISBN}");
+        WriteLine($"autor: {registro.Autor}");
+        WriteLine($"assunto: {registro.Assunto}");
+        WriteLine($"valor: {registro.Valor}");
+        WriteLine("---------&---------\n");
+    } 
+}
 
 static void LerCsvComClasse()
 {
