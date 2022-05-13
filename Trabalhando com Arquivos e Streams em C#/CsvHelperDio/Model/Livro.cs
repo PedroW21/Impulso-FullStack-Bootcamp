ffac81efd64456ba112de401edeb5dd312aaedee
Pedro Verner <pedrow21br@gmail.com>
2022-05-13 15:52:02 -0300

    Usando um macete para poder utilizar nomes diferentes no csv e no model

--- Trabalhando com Arquivos e Streams em C#/CsvHelperDio/Model/Livro.cs
+++ Trabalhando com Arquivos e Streams em C#/CsvHelperDio/Model/Livro.cs
@@ -1,10 +1,21 @@
+using CsvHelper.Configuration.Attributes;
+
 public class Livro
 {
+    [Name("titulo")]
     public string Titulo { get; set; }
+
+    [Name("isbn")]
     public string Isbn { get; set; }
+
+    [Name("autor")]
     public string Autor { get; set; }
+
+    [Name("assunto")]
     public string Assunto { get; set; }
+
+    [Name("valor")]
     public string Valor { get; set; }
 
-
+    //caso tivesse uma data, seria o seguinte [Name("dd/MM/yyyy")]
 }
