d1a9f7fd9b6d7dec2da575d79b6b6ac4d418ef64
Pedro Verner <pedrow21br@gmail.com>
2022-05-13 14:47:07 -0300

    Melhorando nosso codigo: utilizando uma classe no lugar de dynamic

---
+++ Trabalhando com Arquivos e Streams em C#/CsvHelperDio/Model/Usuario.cs
@@ -0,0 +1,7 @@
+public class Usuario
+{
+    public string Nome { get; set; }
+    public string Email { get; set; }
+    public int Telefone { get; set; }
+
+}
--- Trabalhando com Arquivos e Streams em C#/CsvHelperDio/Program.cs
+++ Trabalhando com Arquivos e Streams em C#/CsvHelperDio/Program.cs
@@ -3,26 +3,60 @@ using CsvHelper.Configuration;
 using CsvHelper;
 using static System.Console;
 
-var path = Path.Combine(Environment.CurrentDirectory, "Entrada", "Produtos.csv");
+// LerCsvDinamyc();
 
-var fi = new FileInfo(path);
-
-if(!fi.Exists) throw new FileNotFoundException($"O arquivo {path} não existe!!!");
-
-using var sr = new StreamReader(fi.FullName);
-
-var csvConfig = new CsvConfiguration(CultureInfo.InvariantCulture);
-using var csvReader = new CsvReader(sr, csvConfig);
-
-var registros = csvReader.GetRecords<dynamic>();
-
-foreach (var registro in registros)
-{
-    WriteLine($"nome: {registro.nome}");
-    WriteLine($"marca: {registro.marca}");
-    WriteLine($"preço: {registro.preco}");
-    WriteLine("---------&---------\n");
-}
+LerCsvComClasse();
 
 WriteLine("Pressione ENTER para sair...");
 ReadLine();
+
+static void LerCsvComClasse()
+{
+    var path = Path.Combine(Environment.CurrentDirectory, "Entrada", "novos-usuarios.csv");
+    var fi = new FileInfo(path);
+    if (!fi.Exists) throw new FileNotFoundException($"O arquivo {path} não existe!!!");
+
+    using var sr = new StreamReader(fi.FullName);
+
+    var csvConfig = new CsvConfiguration(CultureInfo.InvariantCulture);
+    using var csvReader = new CsvReader(sr, csvConfig);
+
+    var registros = csvReader.GetRecords<Usuario>();
+
+    foreach (var registro in registros)
+    {
+        WriteLine($"nome: {registro.Nome}");
+        WriteLine($"email: {registro.Email}");
+        WriteLine($"telefone: {registro.Telefone}");
+        WriteLine("---------&---------\n");
+    }
+}
+
+
+static void lerCsvDinamyc()
+{
+    var path = Path.Combine(Environment.CurrentDirectory, "Entrada", "Produtos.csv");
+
+    var fi = new FileInfo(path);
+
+    if (!fi.Exists) throw new FileNotFoundException($"O arquivo {path} não existe!!!");
+
+    using var sr = new StreamReader(fi.FullName);
+
+    var csvConfig = new CsvConfiguration(CultureInfo.InvariantCulture);
+    using var csvReader = new CsvReader(sr, csvConfig);
+
+    var registros = csvReader.GetRecords<dynamic>();
+
+    foreach (var registro in registros)
+    {
+        WriteLine($"nome: {registro.nome}");
+        WriteLine($"marca: {registro.marca}");
+        WriteLine($"preço: {registro.preco}");
+        WriteLine("---------&---------\n");
+    }
+}
+
+
+
+
