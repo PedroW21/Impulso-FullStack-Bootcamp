ef0d48f1bc891bb65b3ebfd1beb0f18bbc7f1fd0
Pedro Verner <pedrow21br@gmail.com>
2022-05-13 09:39:36 -0300

    Metodo para grandes textos

--- Trabalhando com Arquivos e Streams em C#/Stream_Writer_Reader/Program.cs
+++ Trabalhando com Arquivos e Streams em C#/Stream_Writer_Reader/Program.cs
@@ -1,2 +1,27 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using static System.Console;
+using System.Text;
+
+var sb = new StringBuilder();
+
+sb.AppendLine("Essa é a primeira linha desse texto");
+sb.AppendLine("Chegou a 2a linha desse texto");
+sb.AppendLine("E por ultimo mas não menos importante, a 3a e ultima linha");
+
+using var sr = new StringReader(sb.ToString());
+
+var buffer = new char[10];
+
+var tamanho = buffer.Length;
+
+do
+{
+    buffer = new char[10]; // zera o buffer para (limpa)
+
+    tamanho = sr.Read(buffer);
+    WriteLine(string.Join("", buffer)); // se eu usasse o Write somente, ele mostraria normal e não quebraria a cada 10 char
+
+} while (tamanho >= buffer.Length);
+
+WriteLine("Pressione ENTER para finalizar...");
+ReadLine();
+
