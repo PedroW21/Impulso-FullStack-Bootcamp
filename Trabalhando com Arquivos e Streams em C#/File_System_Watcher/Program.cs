bca9be91d48f78c93c9648c629a61dffa0b24b7c
Pedro Verner <pedrow21br@gmail.com>
2022-05-13 09:13:24 -0300

    Fim

--- Trabalhando com Arquivos e Streams em C#/File_System_Watcher/Program.cs
+++ Trabalhando com Arquivos e Streams em C#/File_System_Watcher/Program.cs
@@ -1,2 +1,35 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using static System.Console;
+
+var path = @"/home/pedro/Documentos/Terra";
+
+using var fsw = new FileSystemWatcher(path);
+
+fsw.Created += OnCreated;
+fsw.Deleted += OnDeleted;
+fsw.Renamed += OnRenamed;
+
+fsw.EnableRaisingEvents = true;
+fsw.IncludeSubdirectories = true;
+
+WriteLine($"Monitorando eventos na pasta {path}");
+
+WriteLine("Pressione ENTER para finalizar...");
+ReadLine();
+
+void OnCreated(object sender, FileSystemEventArgs e)
+{
+    WriteLine($"Foi criado o arquivo {e.Name}");
+}
+
+
+void OnDeleted(object sender, FileSystemEventArgs e)
+{
+    WriteLine($"Foi excluido o arquivo {e.Name}");
+}
+
+
+void OnRenamed(object sender, RenamedEventArgs e)
+{
+    WriteLine($"O arquivo {e.OldName} foi renomeado para {e.Name}");
+}
+
