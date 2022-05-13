d983a615969fb4680cfd0a852a5fc12661e3a242
Pedro Verner <pedrow21br@gmail.com>
2022-05-13 13:30:28 -0300

    Lendo e mostrando um arquivo csv em um programa

---
+++ Trabalhando com Arquivos e Streams em C#/CsvStream/Program.cs
@@ -0,0 +1,23 @@
+﻿using static System.Console;
+
+var path = Path.Combine(Environment.CurrentDirectory, "Entrada","usuarios-exportacao.csv");
+
+using var sr = new StreamReader(path);
+
+var cabecalho = sr.ReadLine()?.Split(","); // ? = se for nulo, ignore a proxima instrução
+
+while(true) {
+    var registro = sr.ReadLine()?.Split(",");
+
+    if(registro == null) break;
+
+    for (int i = 0; i < registro.Length; i++)
+    {
+        WriteLine($"{cabecalho?[i]}:{registro[i]}");        
+    }
+
+    WriteLine("----------&----------");
+}
+
+WriteLine("\n\nPressione ENTER para sair...");
+ReadLine();
