0cc35898a01072f0b8a502e49ca1394a758353d5
Pedro Verner <pedrow21br@gmail.com>
2022-05-17 12:32:39 -0300

    Fim - usado delegate e um delegate multi cast

---
+++ Construtores, Propriedades, Delegates e Eventos em .NET/Modulo01/Models/Calculadora.cs
@@ -0,0 +1,12 @@
+public class Calculadora
+{
+    public static void Somar(int x, int y)
+    {
+        Console.WriteLine($"Soma: {x+y}");
+    }
+
+    public static void Subtrair(int x, int y)
+    {
+        Console.WriteLine($"Subtrair: {x-y}");
+    }
+}
--- Construtores, Propriedades, Delegates e Eventos em .NET/Modulo01/Program.cs
+++ Construtores, Propriedades, Delegates e Eventos em .NET/Modulo01/Program.cs
@@ -1,44 +1,62 @@
 ﻿using static System.Console;
 
-
-
-
-static void ExemploModificadores()
-{   
-    //readonly esta na classe Pessoa
-    const double pi = 3.14;
-    WriteLine(pi);
-    // pi = 0; daria erro por ser const
-    // const double pi; daria erro pois precisa ter valor em sua inicialização
-}
-
-static void ExemploGetAndSet()
+class Program
 {
-Data data = new Data();
+    public delegate void Operacao(int x, int y); // delegate me obrigou a colocar toda a estrutura padrao novamente
 
-// Get e set feito por nos
-//data.SetMes(20);
-//data.MostraMesValido();
+    static void Main(string[] args)
+    {
+        
 
-// Get and Set - outro meio
+        static void ExemploDelegates()
+        {
+            // Operacao op = Operacao(Calculadora.Somar); - outra maneira 
+            Operacao op = new Operacao(Calculadora.Somar);
+            op += Calculadora.Subtrair; // Muti Cast Delegate - pacote dois em um (resumindp)
 
-data.Mes = 7;
-data.MostraMesValido();
+            // op(10,10) - outro jeito
+            op.Invoke(10, 12);
+        }
 
-}
+        static void ExemploModificadores()
+        {
+            //readonly esta na classe Pessoa
+            const double pi = 3.14;
+            WriteLine(pi);
+            // pi = 0; daria erro por ser const
+            // const double pi; daria erro pois precisa ter valor em sua inicialização
+        }
 
-static void ExemploConstrutores()
-{
-    Aluno aluno = new Aluno("Santo", "Andre", "Geografia");
-    Pessoa p1 = new Pessoa("Pierre", "Danté");
+        static void ExemploGetAndSet()
+        {
+            Data data = new Data();
 
-    p1.Mostrar();
+            // Get e set feito por nos
+            //data.SetMes(20);
+            //data.MostraMesValido();
 
-    //Log log = Log.GetInstance();
+            // Get and Set - outro meio
 
-    //log.PropriedadeLog = "Teste Instancia";
+            data.Mes = 7;
+            data.MostraMesValido();
 
-    //Log log2 = Log.GetInstance();
+        }
 
-    //WriteLine(log2.PropriedadeLog);
+        static void ExemploConstrutores()
+        {
+            Aluno aluno = new Aluno("Santo", "Andre", "Geografia");
+            Pessoa p1 = new Pessoa("Pierre", "Danté");
+
+            p1.Mostrar();
+
+            //Log log = Log.GetInstance();
+
+            //log.PropriedadeLog = "Teste Instancia";
+
+            //Log log2 = Log.GetInstance();
+
+            //WriteLine(log2.PropriedadeLog);
+        }
+
+    }
 }
