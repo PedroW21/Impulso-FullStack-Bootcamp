16e028242d4ab9b4e236ae39be0967c71bd89db8
Pedro Verner <pedrow21br@gmail.com>
2022-05-17 15:17:50 -0300

    Fim - 2o pilar de OO - encapsulamento

---
+++ Programando com Orientação a Objetos com C#/Models/Retangulo.cs
@@ -0,0 +1,36 @@
+public class Retangulo
+{
+    private double comprimento { get; set; }
+
+    private double largura { get; set; }
+
+    private bool valido;
+
+    public void DefinirMedidas(double comprimento, double largura)
+    {
+        if (comprimento > 0 && largura > 0)
+        {
+            this.comprimento = comprimento;
+            this.largura = largura;
+            valido = true;
+        }
+        else
+        {
+            System.Console.WriteLine("Valores inválidos!");
+        }
+
+    }
+
+    public double ObterArea()
+    {
+        if (valido)
+        {
+            return comprimento * largura;
+        }
+        else
+        {
+            System.Console.WriteLine("Insira valores válidos!!");
+            return(0);
+        }
+    }
+}
--- Programando com Orientação a Objetos com C#/Program.cs
+++ Programando com Orientação a Objetos com C#/Program.cs
@@ -1,8 +1,25 @@
 ﻿using static System.Console;
 
-Pessoa p1 = new Pessoa();
 
-p1.Nome = "Iriney";
-p1.Idade = 17;
 
-p1.Mostrar();
+
+static void ExemploEncapsulamento()
+{ // Propriedades inacessiveis (privadas), sendo "alterável" por meio de get e set e se forem válidos
+    Retangulo r = new Retangulo();
+
+    // Valores válidos
+    r.DefinirMedidas(10, 10);
+
+    WriteLine($"Área: {r.ObterArea()}m²");
+
+}
+
+static void ExemploAbstracao()
+{
+    Pessoa p1 = new Pessoa();
+
+    p1.Nome = "Iriney";
+    p1.Idade = 17;
+
+    p1.Mostrar();
+}
