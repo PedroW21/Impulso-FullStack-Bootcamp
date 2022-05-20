932ad0a611448f28ccd6054c9dc9b5e032c7e9d6
Pedro Verner <pedrow21br@gmail.com>
2022-05-20 09:26:09 -0300

    Fim do modulo 1 - Criando um array simples e multidimensional, iterando-os e populando

--- Fundamentos de Coleções e LINQ com .NET/Program.cs
+++ Fundamentos de Coleções e LINQ com .NET/Program.cs
@@ -1,2 +1,47 @@
-﻿
-Console.WriteLine("Hello, World!");
+﻿using static System.Console;
+
+
+
+
+static void ArrayMatriz()
+{
+    int[,] matriz = new int[4, 2]
+    {
+    { 8 , 8 },
+    { 10 , 20 },
+    { 100 , 200 },
+    { 50 , 500 }
+    };
+
+    for (int i = 0; i < matriz.GetLength(0); i++)
+    {
+        for (int j = 0; j < matriz.GetLength(1); j++)
+        {
+            WriteLine(matriz[i, j]);
+        }
+    }
+
+}
+
+static void ArraySimples()
+{
+    int[] arrayInt = new int[3]; // array que guarda 3 elemtos | Declarando o array em C#
+
+    arrayInt[0] = 128;
+    arrayInt[1] = 256;
+    arrayInt[2] = 512;
+
+    // arrayInt[3] = 30; dara erro em tempo de execução
+    // arrayInt[2] = "1"; - dara erro pelo intellisense (e na execução tb, mas o intelissense ja avisa)
+    // correto = arrayInt[2] = int.Parse("1");
+
+    for (int i = 0; i < arrayInt.Length; i++)
+    {
+        WriteLine($"For: {arrayInt[i]}");
+    }
+
+    foreach (var item in arrayInt)
+    {
+        WriteLine($"Foreach: {item}");
+    }
+}
