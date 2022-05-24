9293f3880ee1da29488a97491411810d591d20b1
Pedro Verner <pedrow21br@gmail.com>
2022-05-24 15:48:05 -0300

    Arrays: Ordenando, auto e na mão | Copiando, Encontrando elements e indx, resizing and converting

--- Fundamentos de Coleções e LINQ com .NET/Helper/OperacoesArray.cs
+++ Fundamentos de Coleções e LINQ com .NET/Helper/OperacoesArray.cs
@@ -1,3 +1,5 @@
+using System;
+
 public class OperacoesArray
 {
     public void OrdernarBubbleSort(ref int[] array)
@@ -21,9 +23,47 @@ public class OperacoesArray
 
     public void ImprimirArray(int[] array)
     {
-        for (int i = 0; i < array.Length; i++)
-        {
-            Console.WriteLine(array[i]);
-        }
+        var linha = string.Join(", ", array);
+        Console.WriteLine(linha);
+    }
+
+    public void OrdenarComClasseArray(ref int[] array)
+    {
+        Array.Sort(array);
+    }
+
+    public void CopiaArray(ref int[] array, ref int[] arrayDestino)
+    {
+        Array.Copy(array, arrayDestino, array.Length);
+    }
+
+    public bool Existe(int[] array, int valor)
+    {
+        return Array.Exists(array, elemento => elemento == valor);
+    }
+
+    public bool TodosMaiorQue(int[] array, int valor)
+    {
+        return Array.TrueForAll(array, elemento => elemento > valor);
+    }
+
+    public int ObterValor(int[] array, int valor)
+    {
+        return Array.Find(array, elemento => elemento == valor);
+    }
+
+    public int ObterIndice(int[] array, int valor)
+    {
+        return Array.IndexOf(array, valor);
+    }
+
+    public void RedimensionarArray(ref int[] array, int novoTamanho)
+    {
+        Array.Resize(ref array, novoTamanho);
+    }
+
+    public string[] ConverterArrayParaString(int[] array)
+    {
+        return Array.ConvertAll(array, elemento => elemento.ToString());
     }
 }
--- Fundamentos de Coleções e LINQ com .NET/Program.cs
+++ Fundamentos de Coleções e LINQ com .NET/Program.cs
@@ -1,18 +1,149 @@
 ﻿using static System.Console;
 
-OrdenandoComBubbleSort();
+ConvertendoTipoArray();
+
+static void ConvertendoTipoArray()
+{
+    OperacoesArray op = new OperacoesArray();
+
+    int[] arr = new int[5] { 3, 8, 9, 1, 23 };
+
+    string[] arrayString = op.ConverterArrayParaString(arr);
+
+}
+
+static void RedimensionaArray()
+{
+    OperacoesArray op = new OperacoesArray();
+
+    int[] arr = new int[5] { 3, 8, 9, 1, 23 };
+
+    //int valor = 10;
+    
+    WriteLine($"Capacidade atual do array: {arr.Length}");
+
+    op.RedimensionarArray(ref arr, arr.Length*2);
+
+    WriteLine($"Capacidade modificada do array: {arr.Length}");
+
+}
+
+static void EncontraIndiceElemento()
+{
+    OperacoesArray op = new OperacoesArray();
+
+    int[] arr = new int[5] { 3, 8, 9, 1, 23 };
+
+    int valorProcurado = 9;
+
+    int indice = op.ObterIndice(arr, valorProcurado);
+
+    if (indice > -1)
+    {
+        WriteLine("O indice do elemento {0} é: {1}", valorProcurado, indice);
+    }
+    else
+    {
+        WriteLine("Valor não existente no array");
+    }
+}
+
+static void EncontraElementoArray()
+{
+    OperacoesArray op = new OperacoesArray();
+
+    int[] arr = new int[5] { 3, 8, 9, 1, 23 };
+
+    int valorProcurado = 9;
+
+    int valorAchado = op.ObterValor(arr, valorProcurado);
+
+    if (valorAchado > 0)
+    {
+        WriteLine("Encontrei o valor");
+    }
+    else
+    {
+        WriteLine("Não encontrei o valor");
+    }
+}
+
+static void VerificandoTodosElementosArray()
+{
+    OperacoesArray op = new OperacoesArray();
+
+    int[] arr = new int[5] { 3, 8, 9, 1, 23 };
+
+    int valorProcurado = 5;
+
+    bool todosMaiorQue = op.TodosMaiorQue(arr, valorProcurado);
+
+    if (todosMaiorQue)
+    {
+        WriteLine($"Todos são maior que o valor desejado | Valor: {valorProcurado}");
+    }
+    else
+    {
+        WriteLine($"Existe valores que não são maiores que o valor procurado | Valor: {valorProcurado}");
+    }
+}
+
+static void EncontraElementoProcuradoEmArray()
+{
+    OperacoesArray op = new OperacoesArray();
+
+    int[] arr = new int[5] { 3, 8, 9, 1, 23 };
+
+    int valorProcurado = 10;
+    bool existe = op.Existe(arr, valorProcurado);
+
+    if (existe)
+    {
+        WriteLine($"Encontrei o valor procurado!!! É: {valorProcurado}");
+    }
+    else
+    {
+        WriteLine("Vai da não");
+    }
+}
+
+static void OrdenarArrayComClassArray()
+{
+    OperacoesArray op = new OperacoesArray();
+
+    int[] arr = new int[5] { 3, 8, 9, 1, 23 };
+
+    WriteLine("--------------- Copiando um array ---------------");
+    int[] arrCopy = new int[10];
+    WriteLine("Array antes da copia: ");
+    op.ImprimirArray(arrCopy);
+    op.CopiaArray(ref arr, ref arrCopy);
+    WriteLine("Array depois da copia: ");
+    op.ImprimirArray(arrCopy);
+    WriteLine("--------------- FIM ---------------");
+}
 
 static void OrdenandoComBubbleSort()
 {
     OperacoesArray op = new OperacoesArray();
-    
-    int[] arr = new int[5]{3,8,9,1,23};
+
+    int[] arr = new int[5] { 3, 8, 9, 1, 23 };
+
+    WriteLine("--------------- Copiando um array ---------------");
+    int[] arrCopy = new int[10];
+    WriteLine("Array antes da copia: ");
+    op.ImprimirArray(arrCopy);
+    op.CopiaArray(ref arr, ref arrCopy);
+    WriteLine("Array depois da copia: ");
+    op.ImprimirArray(arrCopy);
+    WriteLine("--------------- FIM ---------------");
 
     WriteLine("Array Original: \n");
     op.ImprimirArray(arr);
 
     WriteLine("\nArray ordenado: \n");
-    op.OrdernarBubbleSort(ref arr);
+    // op.OrdernarBubbleSort(ref arr); // a classe array faz isso por baixo dos panos no metodo sort
+    op.OrdenarComClasseArray(ref arr);
     op.ImprimirArray(arr);
 
 }
