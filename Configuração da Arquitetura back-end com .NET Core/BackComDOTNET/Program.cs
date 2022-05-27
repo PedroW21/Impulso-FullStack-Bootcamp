bb24b82452abd093d6a5b0e66ad9431ebe9db469
Pedro Verner <53663456+PedroW21@users.noreply.github.com>
2022-05-27 09:00:54 -0300

    Utilizando Swagger e uma lib para melhorar o entendimento dos status code

--- Configuração da Arquitetura back-end com .NET Core/BackComDOTNET/Program.cs
+++ Configuração da Arquitetura back-end com .NET Core/BackComDOTNET/Program.cs
@@ -1,3 +1,5 @@
+using System.Reflection;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -5,7 +7,12 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(c =>
+{
+    var xmlFile = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
+    var xmlPath = Path.Combine(AppContext.BaseDirectory, xmlFile);
+    c.IncludeXmlComments(xmlPath);
+});
 
 var app = builder.Build();
 
@@ -21,5 +28,13 @@ app.UseHttpsRedirection();
 app.UseAuthorization();
 
 app.MapControllers();
+
+app.UseSwagger();
+
+app.UseSwaggerUI(c =>
+{
+    c.SwaggerEndPoint("/swagger/v1/swagger.json", "Api curso");
+    c.RoutePrefix = string.Empty; // swagger
+});
 
 app.Run();
